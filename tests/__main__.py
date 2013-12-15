ca29bd6cd770c47bdca86600e2daee63b4000745
Cacilhας, La Batalema <batalema@cacilhas.info>
2013-12-14 23:41:37 -0200

    modularizing tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
 # coding: UTF-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 # @copyright ©2013, Rodrigo Cacilhας <batalema@cacilhas.info>
 
 from os import path
+import sys
+
+sys.path.append(path.realpath(path.join(path.dirname(__file__), path.pardir)))
+
 from operator import index
 from unittest import main, skip, TestCase
 from object_proxy import Proxy
@@ -12,17 +15,17 @@ from object_proxy import Proxy
 class TestProxy(TestCase):
 
     def setUp(self):
-        self.testp = Proxy('testp')
-        self.num = Proxy('testp:num')
-        self.mylist = Proxy('testp:mylist')
-        self.X = Proxy('testp:X')
+        self.fixtures = Proxy('tests.fixtures')
+        self.num = Proxy('tests.fixtures:num')
+        self.mylist = Proxy('tests.fixtures:mylist')
+        self.X = Proxy('tests.fixtures:X')
 
 
     def test_all(self):
-        self.assertEqual(self.testp.__all__, ['num', 'X'])
+        self.assertEqual(self.fixtures.__all__, ['num', 'X'])
 
     def test_bases(self):
-        base = Proxy('testp:base')
+        base = Proxy('tests.fixtures:base')
         self.assertEqual(self.X.__bases__, (base, ))
 
     def test_dict(self):
@@ -33,13 +36,13 @@ class TestProxy(TestCase):
 
     def test_file(self):
         self.assertTrue(
-            path.basename(self.testp.__file__) in ('testp.py', 'testp.pyc'))
+            path.basename(self.fixtures.__file__) in ('fixtures.py', 'fixtures.pyc'))
 
     def test_name(self):
         self.assertEqual(self.X.__name__, 'X')
 
     def test_package(self):
-        self.assertTrue(self.testp.__package__ is None)
+        self.assertTrue(self.fixtures.__package__ is None)
 
     def test_abs(self):
         self.assertEqual(abs(self.num), 23)
@@ -54,7 +57,7 @@ class TestProxy(TestCase):
         self.assertTrue(bool(self.num))
 
     def test_call(self):
-        incr = Proxy('testp:incr')
+        incr = Proxy('tests.fixtures:incr')
         self.assertEqual(incr(2), 3)
 
     def test_cmp(self):
@@ -78,13 +81,13 @@ class TestProxy(TestCase):
         pass
 
     def test_delitem(self):
-        mydict = Proxy('testp:mydict')
+        mydict = Proxy('tests.fixtures:mydict')
         assert 'a' in mydict
         del mydict['a']
         self.assertFalse('a' in mydict)
 
     def test_delslice(self):
-        mutablestring = Proxy('testp:mutablestring')
+        mutablestring = Proxy('tests.fixtures:mutablestring')
         assert mutablestring == ['a', 'b', 'c', 'd', 'e', 'f']
         del mutablestring[1:3]
         self.assertEqual(mutablestring, ['a', 'd', 'e', 'f'])
@@ -231,8 +234,8 @@ class TestProxy(TestCase):
     def test_repr(self):
         self.assertEqual(repr(self.num), '<Proxy to 23>')
         self.assertEqual(repr(self.mylist), '<Proxy to [0, 1, 2, 3]>')
-        self.assertEqual(repr(self.testp),
-                         "<Proxy to <module 'testp' from '{}'>>".format(self.testp.__file__))
+        self.assertEqual(repr(self.fixtures),
+                         "<Proxy to <module 'tests.fixtures' from '{}'>>".format(self.fixtures.__file__))
 
     def test_reversed(self):
         self.assertEqual([x for x in reversed(self.mylist)], [3, 2, 1, 0])
@@ -276,18 +279,18 @@ class TestProxy(TestCase):
         pass
 
     def test_setattr(self):
-        x = Proxy('testp:changeable_x')
+        x = Proxy('tests.fixtures:changeable_x')
         setattr(x, 'set_attr', 'set')
         self.assertEqual(x.set_attr, 'set')
 
     def test_setitem(self):
-        l = Proxy('testp:changeable_list_1')
+        l = Proxy('tests.fixtures:changeable_list_1')
         assert l == [0, 1]
         l[1] = 2
         self.assertEqual(l, [0, 2])
 
     def test_setslice(self):
-        l = Proxy('testp:changeable_list_2')
+        l = Proxy('tests.fixtures:changeable_list_2')
         assert l == [0, 1, 2, 3]
         l[1:3] = [4]
         self.assertEqual(l, [0, 4, 3])
@@ -305,7 +308,7 @@ class TestProxy(TestCase):
         self.assertEqual(self.num - 2, 21)
 
     def test_subclasshook(self):
-        base = Proxy('testp:base')
+        base = Proxy('tests.fixtures:base')
         self.assertTrue(issubclass(self.X, base._target))
 
     def test_truediv(self):
