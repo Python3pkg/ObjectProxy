fa1c34fd057aa3ad12f6d281aadca459e5f627ca
Cacilhας, La Batalema <batalema@cacilhas.info>
2013-12-16 10:10:55 -0200

    protect non-importable classes from monkey patch

--- object_proxy/lazy.py
+++ object_proxy/lazy.py
@@ -10,108 +10,114 @@ from .context import Context
 __all__ = ['LazyProxy']
 
 
-class ProxyBase(object):
+@apply
+def LazyProxy():
 
-    def __init__(self, targetname, context=None):
-        ProxyBase.__setattr__(self, '__context', None)
-        ProxyBase.__setattr__(self, '__is_weakref', False)
-        context = Context(context or Context.current)
-        context.register(self, targetname)
+    class ProxyBase(object):
 
-
-    def __import_target(self):
-        context = Context.current
-        ProxyBase.__setattr__(self, '__context', context)
-        name = context[id(self)]
-        if ':' in name:
-            name, objname = name.split(':', 1)
-        else:
-            objname = None
-
-        module = import_module(name)
-        target = getattr(module, objname) if objname else module
-
-        try:
-            target = weakref(target)
-        except TypeError:
+        def __init__(self, targetname, context=None):
+            ProxyBase.__setattr__(self, '__context', None)
             ProxyBase.__setattr__(self, '__is_weakref', False)
-        else:
-            ProxyBase.__setattr__(self, '__is_weakref', True)
-
-        ProxyBase.__setattr__(self, '__target', target)
+            context = Context(context or Context.current)
+            context.register(self, targetname)
 
 
-    @property
-    def _target(self):
-        context = ProxyBase.__getattribute__(self, '__context')
-        if context != Context.current:
-            ProxyBase.__import_target(self)
-        target = ProxyBase.__getattribute__(self, '__target')
-        if ProxyBase.__getattribute__(self, '__is_weakref'):
-            target = target()
-        return target
+        def __import_target(self):
+            context = Context.current
+            ProxyBase.__setattr__(self, '__context', context)
+            name = context[id(self)]
+            if ':' in name:
+                name, objname = name.split(':', 1)
+            else:
+                objname = None
+
+            module = import_module(name)
+            target = getattr(module, objname) if objname else module
+
+            try:
+                target = weakref(target)
+            except TypeError:
+                ProxyBase.__setattr__(self, '__is_weakref', False)
+            else:
+                ProxyBase.__setattr__(self, '__is_weakref', True)
+
+            ProxyBase.__setattr__(self, '__target', target)
 
 
-class ProxyMeta(type):
-
-    props = (
-        '__all__', '__doc__', '__file__', '__name__', '__package__',
-    )
-
-    meths = (
-        '__delete__', '__delitem__', '__delslice__', '__div__',
-        '__enter__', '__exit__', '__get__', '__index__', '__nonzero__',
-        '__set__', '__setattr__', '__setitem__', '__setslice__',
-        '__sizeof__', '__subclasshook__',
-    )
+        @property
+        def _target(self):
+            context = ProxyBase.__getattribute__(self, '__context')
+            if context != Context.current:
+                ProxyBase.__import_target(self)
+            target = ProxyBase.__getattribute__(self, '__target')
+            if ProxyBase.__getattribute__(self, '__is_weakref'):
+                target = target()
+            return target
 
 
-    def __new__(metaclass, name, bases, dct):
-        for prop in metaclass.props:
-            dct[prop] = metaclass.build_property(prop)
+    class ProxyMeta(type):
 
-        for meth in metaclass.meths:
-            dct[meth] = metaclass.build_proxy_method(meth)
+        props = (
+            '__all__', '__doc__', '__file__', '__name__', '__package__',
+        )
 
-        for meth in method_map.iterkeys():
-            dct[meth] = metaclass.build_special_method(meth)
-
-        return type(name, bases, dct)
-
-
-    @staticmethod
-    def build_property(prop):
-        return property(
-            lambda self: getattr(super(LazyProxy, self)._target, prop)
+        meths = (
+            '__delete__', '__delitem__', '__delslice__', '__div__',
+            '__enter__', '__exit__', '__get__', '__index__', '__nonzero__',
+            '__set__', '__setattr__', '__setitem__', '__setslice__',
+            '__sizeof__', '__subclasshook__',
         )
 
 
-    @staticmethod
-    def build_proxy_method(meth):
-        return (
-            lambda self, *args:
-                getattr(super(LazyProxy, self)._target, meth)(*args)
-        )
+        def __new__(metaclass, name, bases, dct):
+            for prop in metaclass.props:
+                dct[prop] = metaclass.build_property(prop)
+
+            for meth in metaclass.meths:
+                dct[meth] = metaclass.build_proxy_method(meth)
+
+            for meth in method_map.iterkeys():
+                dct[meth] = metaclass.build_special_method(meth)
+
+            return type(name, bases, dct)
 
 
-    @staticmethod
-    def build_special_method(meth):
-        return (
-            lambda self, *args:
-                method_map[meth](super(LazyProxy, self)._target, *args)
-        )
+        @staticmethod
+        def build_property(prop):
+            return property(
+                lambda self: getattr(super(LazyProxy, self)._target, prop)
+            )
 
 
-class LazyProxy(ProxyBase):
+        @staticmethod
+        def build_proxy_method(meth):
+            return (
+                lambda self, *args:
+                    getattr(super(LazyProxy, self)._target, meth)(*args)
+            )
 
-    __metaclass__ = ProxyMeta
-    __dict__ = property(lambda self: vars(super(LazyProxy, self)._target))
 
-    def __repr__(self):
-        context = Context.current
-        try:
-            target = context[id(self)]
-            return "<LazyProxy to '{}'>".format(target)
+        @staticmethod
+        def build_special_method(meth):
+            return (
+                lambda self, *args:
+                    method_map[meth](super(LazyProxy, self)._target, *args)
+            )
 
-        except NameError:
-            return '<LazyProxy from another context>'
+
+    class LazyProxy(ProxyBase):
+
+        __metaclass__ = ProxyMeta
+        __dict__ = property(lambda self: vars(super(LazyProxy, self)._target))
+
+        def __repr__(self):
+            context = Context.current
+            try:
+                target = context[id(self)]
+                return "<LazyProxy to '{}'>".format(target)
+
+            except NameError:
+                return '<LazyProxy from another context>'
+
+
+    return LazyProxy
